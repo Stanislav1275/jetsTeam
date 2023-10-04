ce71fe03daf077e17fd204533132da12be350f1b
COCKTAIL <aristovstas2004@gmail.com>
2023-10-04 23:19:40 +0400

    fix:migrations

---
+++ app/Livewire/Migrate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Migrate extends Component
+{
+    public function render()
+    {
+        return view('livewire.migrate');
+    }
+}
